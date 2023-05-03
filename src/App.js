2dd5d5215682fa20f385429def6ec1c600a1365e
Mambo Matala <v.mambomatala@dxc.com>
2023-05-03 12:44:48 +0200

    1commit ricky&morty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import React,{useEffect, useState} from 'react';
+import Characters from './components/Characters';
+import Pagination from './components/Pagination';
+
 
 function App() {
+
+  const [characters, setCharacters]= useState([]);
+  const [info,setInfo] = useState({});
+  const Initialurl ="https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters= (url) =>{
+    fetch(url)
+      .then(Response =>Response.json())
+      .then(data => { 
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch(error=>console.log(error))
+  };
+
+  const onPrevious = () =>{
+    fetchCharacters(info.prev);
+  }  
+  const onNext = () =>{
+    fetchCharacters(info.next);
+    
+  }
+useEffect(() =>{
+fetchCharacters(Initialurl);
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar brand="Rick and Morty App"/>
+    <div className='container mt-5'>
+      <Pagination prev={info.prev} 
+      next={info.next}
+      onPrevious={onPrevious}
+      onNext={onNext}
+        />
+      <Characters characters={characters} />
+      <Pagination/>
     </div>
+    </>
   );
 }
 
